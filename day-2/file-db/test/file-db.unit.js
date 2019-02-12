c2f0f2f25f9146086f07b34494ab8e0698d263c4
Andrii Chyzh <andriic@wix.com>
2019-02-12 10:38:12 +0200

    Add base tests

---
+++ day-2/file-db/test/file-db.unit.js
@@ -0,0 +1,18 @@
+
+describe('[Unit] file db', () => {
+
+  before(async () => {});
+
+  after(async () => {});
+
+  describe('#insert()', () => {
+
+    it('should created record', async () => {
+
+      true.should.be.eql(true);
+
+    });
+
+  });
+
+});
